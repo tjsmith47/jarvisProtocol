79822b506db02b2dfa43df9bef19d899705ff55a
Taylor Smith <toss45@gmail.com>
2021-08-16 23:38:44 -0700

    saving edits to pull from work

--- single_model_orm/settings.py
+++ single_model_orm/settings.py
@@ -17,7 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
-#INTERNAL_IPS = ['127.0.0.1', '192.168.4.187']
+INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
